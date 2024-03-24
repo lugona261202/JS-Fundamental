446e645af0ecc823c1bff54a243258b541d00b26
lugona261202 <144421058+lugona261202@users.noreply.github.com>
2024-03-24 14:53:17 +0000

    new add on

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use script";// treats all js code as new version
+
+// aler(3+3)//  we are using nodejs not browser , for the time being avoid using this statement
+console.log(3
+    +3)// although javascript omit spaces , still prefer to work on readiblity
+console.log("harshit")
+
+let name="harshit"
+let age = 21
+let isLoggedIn = false
+let state
+
+//number =>2 ^53
+// if a bigger number then prefer bigint datatype
+//string =>can be declared in both single and double quotes , but prefer double quotes
+//boolean=>true/false
+//null=>standalone value
+//undefined =>value has not been assigned yet
+//symbol=> to introduce uniqueness , will be used in react
+
+
+//object=>will be done extensively later
+
+console.log(typeof "hitesh")
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
+/*
+refer to two documentation throughtout
+tc39.es ecma documentation
+mdn documentation
+*/
