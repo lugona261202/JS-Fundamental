aee0d4e38691df467789744f5cc072f6ff3d70e7
lugona261202 <144421058+lugona261202@users.noreply.github.com>
2024-03-24 15:30:55 +0000

    conversion with operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,33 @@ let someNumber =33
 let stringNumber = String (someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//***** operations******
+
+let value =3 
+let negValue = -value
+//this simply makes negValue as -3
+
+// up ahead are some basic mathematics operation
+console.log (2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)// raised to the power
+console.log(2/3)
+console.log(2%3)
+
+let str1 ="hello"
+let str2 = " hitesh"
+
+let str3 = str1+str2 // string concatenation supported
+console.log(str3)// hello hitesh 
+
+console.log( typeof "1"+2)//converted to string
+console.log(typeof 1+"2")//converted to string
+console.log(typeof "1"+2+2)//here all will be converted to string
+console.log(typeof 1+2+"2")//her first addition takes place then string conversion "32"
+
+let gameCounter = 100
+++gameCounter;
+console.log(gamecounter)
+// this use concept of prefix and postfix operator too
