3d55f0a4156e4d586af42ef96673f37ef9114bfd
lugona261202 <144421058+lugona261202@users.noreply.github.com>
2024-03-26 07:23:10 +0000

    switch cases

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,39 @@
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+// general syntax
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("february")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+}
+// just like we matched number we can also match string or any other data type in switch cases
+
+
+
+
+
+
+
